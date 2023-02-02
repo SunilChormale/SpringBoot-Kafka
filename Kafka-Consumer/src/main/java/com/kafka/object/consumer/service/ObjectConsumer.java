89710834bd81e55b6daa11c0b13c8dc8cb34e293
SunilChormale <sunilschormale01@gmail.com>
2023-02-02 22:04:37 +0530

    created comsumer for topic

---
+++ Kafka-Consumer/src/main/java/com/kafka/object/consumer/service/ObjectConsumer.java
@@ -0,0 +1,21 @@
+package com.kafka.object.consumer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ObjectConsumer {
+
+    @Value("${topic.name}")
+    private String TOPIC_NAME;
+
+    private Logger log = LoggerFactory.getLogger(ObjectConsumer.class);
+
+    @KafkaListener(topics = "${topic.name}", groupId = "group1")
+    public void consume(Long object){
+        log.info(String.format("Value received -> %s from the Topic named: %s", object, TOPIC_NAME));
+    }
+}
